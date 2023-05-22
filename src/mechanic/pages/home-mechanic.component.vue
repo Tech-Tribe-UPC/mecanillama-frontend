ba120b3f166b1b01d1a922fa6b76e2ad47dffff1
jrenzzo <u202118568@upc.edu.pe>
2023-05-21 23:15:03 -0500

    feat: display basic data in mechanic profile

--- src/mechanic/pages/home-mechanic.component.vue
+++ src/mechanic/pages/home-mechanic.component.vue
@@ -1,51 +1,36 @@
-<script>
+<script setup>
 import MechanicHeaderComponent from "../../components/mechanic-header.component.vue";
 import appointmentsMechanicComponent from "../../appointments/pages/appointments-mechanic.component.vue";
 import { MechanicsProfileApiService } from "../services/mechanics-api.service";
-export default {
-  name: "home-mechanic",
-  components: {
-    MechanicHeaderComponent,
-    appointmentsMechanicComponent,
-  },
-  data() {
-    return {
-      mechanic: null,
-      mechanicUserService: null,
-      mechanicId: null,
-    };
-  },
-  created() {
-    this.mechanicUserService = new MechanicsProfileApiService();
-    this.mechanicId = localStorage.getItem("user");
-    console.log(this.mechanicId);
-    this.mechanicUserService.getByUserId(this.mechanicId).then((response) => {
-      console.log(response);
-      this.mechanic = response.data;
-    });
-    
-  },
-};
+import { useAuthStore } from "../../stores/auth.store";
+import { computed } from "vue";
+
+const userStore = useAuthStore();
+const auth = computed(() => userStore.user);
 </script>
 
 <template>
   <mechanic-header-component></mechanic-header-component>
 
   <div class="container">
-    <div id="sup" class="container">
-      <img
-        src="https://images.pexels.com/photos/104836/speed-car-vehicle-drive-104836.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-      />
-      <div>
-        <span>
-          <h3 class="mb-1">The Mechanic Shop</h3>
-          <p class="m-1">
-            The Mechanic Shop is your one-stop-shop for all your auto repair needs
-          </p>
-          <router-link to="">
-            <pv-button id="button" label="Edit" />
-          </router-link>
-        </span>
+    <div class="flex flex-column m-4">
+      <div class="flex m-0 m-auto">
+        <img
+          src="https://images.pexels.com/photos/104836/speed-car-vehicle-drive-104836.jpeg?auto=compress&cs=tinysrgb&w=560&h=350&dpr=1"
+          class="w-1/2"
+        />
+        <div class="flex-1 mx-4">
+          <div class="text-center">  
+            <div class="text-3xl font-semibold mb-1">Your business's data:</div>
+            <div class="text-3xl font-semibold mb-1">{{ auth.user.name }}</div>
+            <div class="text-xl m-2">{{ auth.user.address }}</div>
+            <div class="text-xl m-2">{{ auth.user.phone }}</div>
+            <div class="text-xl mx-6 mb-4">
+              {{ auth.user.description }}
+            </div>
+              <q-btn color="black" label="Edit my info" />
+          </div>
+        </div>
       </div>
     </div>
     <div>
@@ -55,29 +40,5 @@ export default {
 </template>
 
 <style>
-#sup {
-  display: flex;
-  height: 300px;
-  background: black;
-  align-items: center;
-}
-#sup img {
-  height: 95%;
-  left: 10%;
-  border-radius: 25px;
-}
-#sup div {
-  display: flex;
-  position: absolute;
-  width: 30%;
-  height: 100%;
-  right: 5%;
-  background: white;
-  opacity: 80%;
-  align-items: center;
-  text-align: center;
-}
-#button {
-  width: 70%;
-}
+
 </style>
